742c853f3ef70530cc429ad459dfa5225a91432c
Anders Höckersten <anders@hockersten.se>
2013-06-30 11:06:24 +0200

    Tabs to spaces

--- RightClickInChat.mod/RightClickInChat.cs
+++ RightClickInChat.mod/RightClickInChat.cs
@@ -5,35 +5,35 @@ using UnityEngine;
 using Mono.Cecil;
 
 namespace RightClickInChat.mod {
-	public class RightClickInChat : BaseMod {
+    public class RightClickInChat : BaseMod {
 
-		//initialize everything here, Game is loaded at this point
+        //initialize everything here, Game is loaded at this point
         public RightClickInChat() {
-		}
+        }
 
 
-		public static string GetName() {
+        public static string GetName() {
             return "RightClickInChat";
-		}
+        }
 
-		public static int GetVersion() {
-			return 1;
-		}
+        public static int GetVersion() {
+            return 1;
+        }
 
-		//only return MethodDefinitions you obtained through the scrollsTypes object
-		//safety first! surround with try/catch and return an empty array in case it fails
-		public static MethodDefinition[] GetHooks (TypeDefinitionCollection scrollsTypes, int version) {
-			return new MethodDefinition[] {};
-		}
+        //only return MethodDefinitions you obtained through the scrollsTypes object
+        //safety first! surround with try/catch and return an empty array in case it fails
+        public static MethodDefinition[] GetHooks(TypeDefinitionCollection scrollsTypes, int version) {
+            return new MethodDefinition[] { };
+        }
 
-		
-		public override bool BeforeInvoke (InvocationInfo info, out object returnValue) {
-			returnValue = null;
-			return false;
-		}
 
-		public override void AfterInvoke (InvocationInfo info, ref object returnValue) {
-			return;
-		}
-	}
+        public override bool BeforeInvoke(InvocationInfo info, out object returnValue) {
+            returnValue = null;
+            return false;
+        }
+
+        public override void AfterInvoke(InvocationInfo info, ref object returnValue) {
+            return;
+        }
+    }
 }
